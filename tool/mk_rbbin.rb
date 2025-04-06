a767709f8d7581e6c88135def68fe19f7ac9eea3
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-04-06 12:24:23 +0900

    Ensure builtin binaries are aligned to ibf_header
    Since IBF result string size should be multiple of 4, this should not
    increase the binary size actually.

--- tool/mk_rbbin.rb
+++ tool/mk_rbbin.rb
@@ -16,13 +16,18 @@ OPTIMIZATION = {
 file = File.basename(ARGV[0], ".rb")
 name = "<internal:#{file}>"
 iseq = RubyVM::InstructionSequence.compile(ARGF.read, name, name, **OPTIMIZATION)
+bin = iseq.to_binary
 puts <<C
 /* -*- C -*- */
 
-static const char #{file}_builtin[] = {
+static const union {
+    unsigned char binary[#{bin.size}];
+    uint32_t align_as_ibf_header;
+} #{file}_builtin = {
+    .binary = {
 C
-iseq.to_binary.bytes.each_slice(8) do |b|
-  print "    ", b.map {|c| "0x%.2x," % c}.join(" ")
+bin.bytes.each_slice(12) do |b|
+  print "        ", b.map {|c| "0x%.2x," % c}.join(" ")
   if $comment
     print " /* ", b.pack("C*").gsub(/([[ -~]&&[^\\]])|(?m:.)/) {
       (c = $1) ? "#{c} " : (c = $&.dump).size == 2 ? c : ". "
@@ -31,6 +36,7 @@ iseq.to_binary.bytes.each_slice(8) do |b|
   puts
 end
 puts <<C
+    }
 };
 
 #include "ruby/ruby.h"
@@ -39,10 +45,10 @@ puts <<C
 void
 Init_#{file}(void)
 {
-    const char *builtin = #{file}_builtin;
+    const char *builtin = (const char *)#{file}_builtin.binary;
     size_t size = sizeof(#{file}_builtin);
     VALUE code = rb_str_new_static(builtin, (long)size);
     VALUE iseq = rb_funcallv(rb_cISeq, rb_intern_const("load_from_binary"), 1, &code);
-    rb_funcallv(iseq, rb_intern_const("eval"), 0, 0);
+    rb_iseq_eval_main(rb_iseqw_to_iseq(iseq));
 }
 C
