faa0e1bab9ce1fae9f1b7b24c3112a9f9977b08e
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-04-23 15:37:03 +0900

    Simplify assertion expression
    Move the static condition outside.

--- vm_core.h
+++ vm_core.h
@@ -1991,7 +1991,7 @@ rb_current_execution_context(bool expect_ec)
 #else
     rb_execution_context_t *ec = native_tls_get(ruby_current_ec_key);
 #endif
-    VM_ASSERT(!expect_ec || ec != NULL);
+    if (expect_ec) VM_ASSERT(ec);
     return ec;
 }
 
