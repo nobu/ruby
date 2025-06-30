1dcba7ebc6fa8281170a114d50aaf30336bf7628
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-06-30 11:46:05 +0900

    [Misc #21458] Add post-install-check

--- common.mk
+++ common.mk
@@ -493,18 +493,25 @@ $(ruby_pc): $(srcdir)/template/ruby.pc.in config.status
 
 INSTALL_ALL = all
 
-install-all: pre-install-all do-install-all post-install-all
+post-install-check: $(LIBRUBY_RELATIVE)-post-install-check
+
+yes-post-install-check:
+no-post-install-check:
+
+install-all: pre-install-all do-install-all $(DOT_WAIT) post-install-all
 pre-install-all:: all pre-install-local pre-install-ext pre-install-gem pre-install-doc
 do-install-all: pre-install-all $(DOT_WAIT) docs
 	$(INSTRUBY) --make="$(MAKE)" $(INSTRUBY_ARGS) --install=$(INSTALL_ALL) $(INSTALL_DOC_OPTS)
-post-install-all:: post-install-local post-install-ext post-install-gem post-install-doc
+post-install-all:: post-install-local post-install-ext post-install-gem post-install-doc \
+	$(DOT_WAIT) $(TEST_RUNNABLE:yes=post-install-check)
 	@$(NULLCMD)
 
-install-nodoc: pre-install-nodoc do-install-nodoc post-install-nodoc
+install-nodoc: pre-install-nodoc do-install-nodoc $(DOT_WAIT) post-install-nodoc
 pre-install-nodoc:: pre-install-local pre-install-ext pre-install-gem
 do-install-nodoc: main pre-install-nodoc
 	$(INSTRUBY) --make="$(MAKE)" $(INSTRUBY_ARGS) --install=$(INSTALL_ALL) --exclude=doc
-post-install-nodoc:: post-install-local post-install-ext post-install-gem
+post-install-nodoc:: post-install-local post-install-ext post-install-gem \
+	$(DOT_WAIT) $(TEST_RUNNABLE:yes=post-install-check)
 
 install-local: pre-install-local do-install-local post-install-local
 pre-install-local:: pre-install-bin pre-install-lib pre-install-man
