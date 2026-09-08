da609c9cff4da1c30685f213cf7f8152d3297bee
Nobuyoshi Nakada <nobu@ruby-lang.org>
2026-09-09 00:49:37 +0900

    Add recursive removal to `File.unlink`
    Allow `File.unlink` and `File.delete` to remove directory trees
    without following symbolic links within them via `recursive: true`.

--- spec/ruby/core/file/unlink_spec.rb
+++ spec/ruby/core/file/unlink_spec.rb
@@ -5,3 +5,47 @@ describe "File.unlink" do
     File.method(:unlink).should == File.method(:delete)
   end
 end
+
+ruby_version_is "4.1" do
+  guard -> {
+    begin
+      File.unlink(recursive: true)
+      true
+    rescue NotImplementedError
+      false
+    end
+  } do
+    describe "File.unlink with recursive: true" do
+      before :each do
+        @tree = tmp('unlink_recursive')
+        mkdir_p "#{@tree}/subdir"
+        touch "#{@tree}/subdir/file"
+      end
+
+      after :each do
+        rm_r @tree
+      end
+
+      it "removes a directory and its contents" do
+        File.unlink(@tree, recursive: true).should == 1
+        File.should_not.exist?(@tree)
+      end
+
+      it "returns the number of arguments, regardless of the tree size" do
+        file = "#{@tree}/subdir/file"
+        File.unlink(file, @tree, recursive: true).should == 2
+      end
+
+      it "raises when the given path does not exist" do
+        -> { File.unlink("#{@tree}/missing", recursive: true) }.should.raise(Errno::ENOENT)
+      end
+
+      it "does not follow symbolic links" do
+        File.symlink('subdir', "#{@tree}/link")
+        File.unlink("#{@tree}/link", recursive: true).should == 1
+        File.should.exist?("#{@tree}/subdir/file")
+        File.should_not.symlink?("#{@tree}/link")
+      end
+    end
+  end
+end
---
+++ test/ruby/test_file_unlink.rb
@@ -0,0 +1,164 @@
+# frozen_string_literal: false
+require 'test/unit'
+require 'tmpdir'
+require 'fileutils'
+
+class TestFileUnlinkRecursive < Test::Unit::TestCase
+  def setup
+    begin
+      File.unlink(recursive: true)
+    rescue NotImplementedError
+      omit "recursive unlink is not supported"
+    end
+    @root = File.realpath(Dir.mktmpdir('ruby-unlink'))
+  end
+
+  def teardown
+    FileUtils.remove_entry(@root) if @root && File.directory?(@root)
+  end
+
+  def test_tree
+    path = File.join(@root, 'tree')
+    FileUtils.mkdir_p("#{path}/a/b")
+    File.write("#{path}/a/file", 'content')
+    File.write("#{path}/.hidden", 'content')
+    assert_equal(1, File.unlink(path, recursive: true))
+    assert_file.not_exist?(path)
+    assert_file.directory?(@root)
+  end
+
+  def test_deep_tree
+    path = File.join(@root, 'tree')
+    FileUtils.mkdir_p(path + '/a' * 100)
+    assert_equal(1, File.unlink(path, recursive: true))
+    assert_file.not_exist?(path)
+  end
+
+  def test_entry_names
+    path = File.join(@root, 'tree')
+    Dir.mkdir(path)
+    ['a', '.a', '..a', '...', 'a' * 200].each do |name|
+      Dir.mkdir("#{path}/#{name}")
+      File.write("#{path}/#{name}/file", 'content')
+    end
+    assert_equal(1, File.unlink(path, recursive: true))
+    assert_file.not_exist?(path)
+  end
+
+  def test_files_and_multiple_paths
+    paths = %w[file tree].map {|name| File.join(@root, name)}
+    File.write(paths[0], 'content')
+    Dir.mkdir(paths[1])
+    assert_equal(2, File.unlink(*paths, recursive: true))
+    paths.each {|path| assert_file.not_exist?(path)}
+    assert_equal(0, File.unlink(recursive: true))
+  end
+
+  def test_default_and_false
+    path = File.join(@root, 'tree')
+    Dir.mkdir(path)
+    assert_raise(Errno::EPERM, Errno::EISDIR) {File.unlink(path)}
+    assert_raise(Errno::EPERM, Errno::EISDIR) {File.unlink(path, recursive: false)}
+    assert_file.directory?(path)
+    file = File.join(path, 'file')
+    File.write(file, 'content')
+    assert_equal(1, File.unlink(file, recursive: false))
+  end
+
+  def test_arguments
+    path = File.join(@root, 'file')
+    File.write(path, 'content')
+    [nil, 1, :true].each do |value|
+      assert_raise(ArgumentError) {File.unlink(path, recursive: value)}
+    end
+    assert_raise(ArgumentError) {File.unlink(path, unknown: true)}
+    assert_raise(TypeError) {File.unlink(path, Object.new, recursive: true)}
+    assert_file.exist?(path)
+    arg = Object.new
+    arg.define_singleton_method(:to_path) {path.freeze}
+    assert_equal(1, File.unlink(arg, recursive: true))
+    assert_equal(File.join(@root, 'file'), path)
+  end
+
+  def test_missing_path
+    assert_raise(Errno::ENOENT) {File.unlink(File.join(@root, 'missing'), recursive: true)}
+    assert_raise(Errno::ENOENT) {File.unlink('', recursive: true)}
+  end
+
+  def test_keyword_hash
+    options = {recursive: true}.freeze
+    assert_equal(0, File.unlink(**options))
+    assert_equal({recursive: true}, options)
+    [{unknown: true}, {recursive: true, unknown: true}].each do |keywords|
+      keywords.freeze
+      error = assert_raise(ArgumentError) {File.unlink(**keywords)}
+      assert_equal('unknown keyword: :unknown', error.message)
+      assert_equal(true, keywords[:unknown])
+    end
+  end
+
+  def test_alias
+    path = File.join(@root, 'tree')
+    Dir.mkdir(path)
+    assert_equal(File.method(:unlink), File.method(:delete))
+    assert_equal(1, File.delete(path, recursive: true))
+  end
+
+  def test_relative_and_trailing_separators
+    Dir.chdir(@root) do
+      FileUtils.mkdir_p('tree/a/b')
+      assert_equal(1, File.unlink('./tree///', recursive: true))
+      assert_file.not_exist?('tree')
+    end
+  end
+
+  def test_reject_root_and_dot
+    File.write(File.join(@root, 'keep'), 'content')
+    Dir.chdir(@root) do
+      ['/', '///', '.', './', '..', '../', "#{@root}/."].each do |path|
+        assert_raise(Errno::EINVAL, Errno::EBUSY) {File.unlink(path, recursive: true)}
+      end
+      assert_equal('content', File.read('keep'))
+    end
+  end
+
+  def test_symlinks
+    outside = File.join(@root, 'outside')
+    tree = File.join(@root, 'tree')
+    Dir.mkdir(outside)
+    Dir.mkdir(tree)
+    File.write("#{outside}/keep", 'content')
+    File.symlink(outside, "#{tree}/link")
+    File.symlink('missing', "#{tree}/dangling")
+    File.symlink('.', "#{tree}/loop")
+    assert_equal(1, File.unlink(tree, recursive: true))
+    assert_equal('content', File.read("#{outside}/keep"))
+    ["#{@root}/link", "#{@root}/link/"].each do |path|
+      File.symlink(outside, "#{@root}/link")
+      assert_equal(1, File.unlink(path, recursive: true))
+      assert_equal('content', File.read("#{outside}/keep"))
+    end
+  end
+
+  def test_nonascii_path
+    path = File.join(@root, '日本語/親/子')
+    FileUtils.mkdir_p(path)
+    File.write("#{path}/ファイル", 'content')
+    assert_equal(1, File.unlink(File.join(@root, '日本語'), recursive: true))
+    assert_file.not_exist?(path)
+  rescue EncodingError => e
+    omit e.message
+  end
+
+  def test_permission_error
+    omit 'requires directory permissions' if Process.euid == 0
+    path = File.join(@root, 'tree')
+    Dir.mkdir(path)
+    File.write("#{path}/keep", 'content')
+    File.chmod(0o500, path)
+    assert_raise(Errno::EACCES) {File.unlink(path, recursive: true)}
+    assert_equal('content', File.read("#{path}/keep"))
+  ensure
+    File.chmod(0o700, path) if path && File.directory?(path)
+  end
+end
